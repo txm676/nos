5430f78384a8368da35f939c09900828ffa03761
Tom Matthews <txm676@gmail.com>
2024-03-17 19:21:51 +0000

    adding testList doc

---
+++ R/testList_data.R
@@ -0,0 +1,9 @@
+#' Unit tests data
+#'
+#' A dataset used in the unit tests. Has four elements, the first three
+#' being input data, and the fourth the output data. All generated using
+#' the python nos package to ensure the results match up.
+#'
+#' @examples
+#' data(testList)
+"testList"
