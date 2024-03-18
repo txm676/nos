9b3628055d2e8d9eabe946bc2667cca631c7e91e
Tom Matthews <txm676@gmail.com>
2024-03-18 13:35:37 +0000

    adding second dataset to testList

--- R/testList_data.R
+++ R/testList_data.R
@@ -1,8 +1,10 @@
 #' Unit tests data
 #'
-#' A dataset used in the unit tests. Has four elements, the first three
-#' being input data, and the fourth the output data. All generated using
-#' the python nos package to ensure the results match up.
+#' Datasets used in the unit tests. Is a list with two elements,
+#' each representing a different dataset. Each dataset is a list
+#' with four elements, the first three being input data, and the
+#' fourth the output data. All generated using the python nos
+#' package to ensure the results match up.
 #'
 #' @examples
 #' data(testList)
